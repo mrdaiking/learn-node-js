2c67542f6d5c5907bc8b5054d0d9639f62e96bd8
daiking <daikynguyen999@gmail.com>
2020-12-22 00:15:21 +0900

    done logic

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
-app.use("view engine", "ejs");
+var items = ["Banana", "Orange", "Apple"];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-   res.render("list", {kindOfDay: today})
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+
+
+    res.render("list", {kindOfDay: day, listItems: items});
+   
 });
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000,  function(){
     console.log("Server started on port 3000");
