01b80c2dfd894de9f61a1d28bf0c79f12502aa85
cuong <cuong@blayn.co.jp>
2020-12-22 12:16:50 +0900

    done code

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname+"/date.js");
 
 const app = express();
 
-var items = ["Banana", "Orange", "Apple"];
+var items = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
-
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
-
-
-
+    let day = date();
     res.render("list", {kindOfDay: day, listItems: items});
    
 });
@@ -30,7 +21,11 @@ app.post("/", function(req, res){
     var item = req.body.newItem;
     items.push(item);
     res.redirect("/");
-})
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {kindOfDay: "Work list", listItems: items});
+});
 
 app.listen(3000,  function(){
     console.log("Server started on port 3000");
