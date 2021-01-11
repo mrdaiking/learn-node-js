57c741a1ca9547fb5bdee98479b53579780b44a7
cuong <cuong@blayn.co.jp>
2021-01-11 18:26:29 +0900

    setup swagger api

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const ejs = require("ejs");
 const date = require(__dirname+"/date.js");
 
 const app = express();
+const port = 5000;
 
 var items = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(express.static("dist"));
 
 app.get("/", function(req, res){
     let day = date();
@@ -27,6 +29,10 @@ app.get("/work", function(req,res){
     res.render("list", {kindOfDay: "Work list", listItems: items});
 });
 
-app.listen(3000,  function(){
-    console.log("Server started on port 3000");
+app.get('/api', (req, res) => {
+    res.sendFile('dist/index.html' , { root : __dirname});
+});
+
+app.listen(port,  function(){
+    console.log("Server started on port ",port);
 })
